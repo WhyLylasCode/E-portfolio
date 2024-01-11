8e751b3d5862a9e2e89c3edfec3140f3d25f316a
Wiame Elalami <wiameelalami02@gmail.com>
2024-01-11 16:44:50 +0100

    Mis a jour formulaire, recherche de stage, et changements de styles

---
+++ form.php
@@ -0,0 +1,87 @@
+<body> 
+    
+    <!--Contact section start-->
+    <section class="contact section" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="section-title padd-15">
+            <h2>Me contacter</h2>
+            </div>        
+        </div>
+        <h3 class="contact-title">Avez vous des questions ?</h3>
+        <h4 class="contact-sub-title">Je suis à votre service</h4>
+        <div class="row">
+
+
+            <!--Contact info item start-->
+        <div class="contact-info-item padd-15">
+            <div class="icon"><i class="fa fa-phone"></i></div>
+            <h4>Appelez-moi</h4>
+            <p> +33 7 51 35 42 37</p>
+        </div>
+        <div class="contact-info-item padd-15">
+            <div class="icon"><i class="fa fa-envelope"></i></div>
+            <h4>Email</h4>
+            <p> wiameelalami02@gmail.com</p>
+        </div>
+        <div class="contact-info-item padd-15">
+            <div class="icon"><i class="fa fa-map-marked-alt"></i></div>
+            <h4>Localisation</h4>
+            <p> Toulouse</p>
+        </div>
+            <!--Contact info item end-->
+        
+        </div>
+        <h3 class="contact-title">Envoie moi un email</h3>
+        <h4 class="contact-sub-title">Je les consultes régulièrement</h4>
+        
+
+            <!--Contact form-->
+<div class="row">
+    <div class="contact-form padd-15">
+        <div class="row">
+            <div class="form-item col-6 padd-15">
+
+        <div class="form-group">
+            <form action="index.php" method="post">
+            <input name="nom" type="text" class="form-control" id="name" placeholder="Nom" required> 
+        </div>
+    </div>
+    <div class="form-item col-6 padd-15">
+    <div class="form-group">
+
+        <input name="email" type="email" class="form-control" id="email" placeholder="Email" required>
+    </div>
+</div>
+</div>
+
+<div class="row">
+<div class="form-item col-12 padd-15">
+    <div class="form-group">
+
+        <input name="sujet" type="text" class="form-control" id="subject" placeholder="Sujet" required>
+    </div>
+</div>
+</div>
+<div class="row">
+    <div class="form-item col-12 padd-15">
+        <div class="form-group">
+
+            <textarea name="message" class="form-control" id="message" placeholder="Message" required></textarea>
+        </div>
+    </div>
+    </div>
+    <div class="row">
+        <div class="form-item col-12 padd-15">
+            <button type="submit" class="btn" onsubmit="">Envoyer
+            </button>
+        </form>
+        </div>
+        </div>
+</div>
+    </div>
+</div>
+</div>
+    </section>
+    <!--Contact section end-->
+</body>
